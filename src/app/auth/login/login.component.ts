4cfa7e0458569f39638cda141be4fd81f143ac92
Wiki-Episteme <118081759+Wiki-EpisTeme@users.noreply.github.com>
2023-10-15 15:35:23 -0500

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
-import { Form, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
 import { Router } from "@angular/router";
 
 @Component({
@@ -8,29 +14,24 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-
   form = this.fb.group({
-    password:['', Validators.required]
-  })
-    
-  constructor (private fb: FormBuilder, private route:Router ){
+    password: ["", Validators.required]
+  });
 
-  }
+  constructor(private fb: FormBuilder, private route: Router) {}
   ngOnInit() {}
 
-  listPassword: string[] = ['1111'];
-  token: string='';
+  listPassword: string[] = ["valen", "juan"];
+  token: string = "";
 
   pasa() {
-    
     const Token = String(this.form.value.password);
-    console.log(String(Token));
     this.token = Token;
 
-    if (Token.includes(this.listPassword[0])){
-      this.route.navigate(['/home']);
-    }else {
-      console.log('Nada que hacer')
+    if (this.listPassword.includes(this.token)) {
+      this.route.navigate(["/home"]);
+    } else {
+      console.log("no");
     }
   }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Route, Router } from "@angular/router";
+import { LoginComponent } from "src/app/auth/login/login.component";
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
+  constructor(private route:Router){}
+  Titulo = ''
+  
 
 }
