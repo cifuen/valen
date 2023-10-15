5fa42fda1905bec851e2cbe00f7427763fbd3e7b
Wiki-Episteme <118081759+Wiki-EpisTeme@users.noreply.github.com>
2023-10-15 00:35:15 -0500

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from "@angular/core";
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  form = new FormControl();
-  Token = 123
-  token = [
-    12,
-    23,
-    34
-  ]
-  
-  if (token = true) {
+
+  form = this.fb.group({
+    password:['', Validators.required]
+  })
     
+  constructor (private fb: FormBuilder, private route:Router ){
+
   }
-  constructor (){}
+  ngOnInit() {}
+
+  listPassword: string[] = ['1111'];
+  token: string='';
+
+  pasa() {
+    
+    const Token = String(this.form.value.password);
+    console.log(String(Token));
+    this.token = Token;
+
+    if (Token.includes(this.listPassword[0])){
+      this.route.navigate(['/home']);
+    }else {
+      console.log('Nada que hacer')
+    }
+  }
 }
